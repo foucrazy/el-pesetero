ca49b7c57e381285300925bf87a3c88eecb9d325
kortatu <kortatu@gmail.com>
2012-10-16 14:18:56 +0200

    UPG: Creamos sólo un usuario admin

--- server/el-pesetero-server/grails-app/conf/BootStrap.groovy
+++ server/el-pesetero-server/grails-app/conf/BootStrap.groovy
@@ -31,9 +31,7 @@ class BootStrap {
 		String password = 'password'
 		def roleAdmin = new Role(authority: 'ROLE_ADMIN').save()
 		def roleUser = new Role(authority: 'ROLE_USER').save()
-		def user = new SecurityUser(username: 'user', password: password, enabled: true).save()
-		def admin = new SecurityUser(username: 'admin', password: password, enabled: true).save()		
-		UserRole.create user, roleUser 
+		def admin = new SecurityUser(username: 'kortatu', password: password, enabled: true).save()		
 		UserRole.create admin, roleUser 
 		UserRole.create admin, roleAdmin, true
 		admin
