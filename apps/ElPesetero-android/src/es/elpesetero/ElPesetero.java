7a64cae8030c84ea4629f7a4f52c69eaa14bbf31
foucrazy <foucrazy@gmail.com>
2012-11-10 12:52:42 +0100

    Organizacion y ajustes de diseño para 320.

--- apps/ElPesetero-android/src/es/elpesetero/ElPesetero.java
+++ apps/ElPesetero-android/src/es/elpesetero/ElPesetero.java
@@ -10,7 +10,7 @@ public class ElPesetero extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/metro2/index.html");
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 
     @Override
